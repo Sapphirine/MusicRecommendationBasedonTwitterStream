44779cbc1c959f7a8be416c788db2f7b5dfda69e
CandaceYu <jy2731@columbia.edu>
2016-12-22 18:04:05 -0500

    Add files via upload

---
+++ Py_clf_evl/evaluation_NB.py
@@ -0,0 +1,107 @@
+import json
+import pandas as pd
+import re, nltk
+import numpy as np
+import pickle
+import enchant
+import random
+from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer  
+from sklearn.cross_validation import train_test_split  
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report    
+from sklearn.model_selection import cross_val_score
+
+tweets_data_path = '/home/candace/Documents/sentiment_analysis_dataset_sample.csv'
+classifier_NaiveBayes_path = '/home/candace/Documents/NaiveBayes.pickle'
+vectorizer_CountVectorizer_path = '/home/candace/Documents/CountVectorizer.pickle'
+
+whole_data_df = pd.read_csv(tweets_data_path, error_bad_lines=False)
+train_data_df = whole_data_df[['Sentiment','SentimentText']]
+
+# based on http://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html#training-a-classifier
+stemmer = PorterStemmer()
+def stem_tokens(tokens, stemmer):
+    stemmed = []
+    for item in tokens:
+        stemmed.append(stemmer.stem(item))
+    return stemmed
+    
+dt = enchant.Dict("en")
+max_dist = 2
+def sp_correct(word):
+    if dt.check(word):
+        return word
+    suggestions = dt.suggest(word)
+    if suggestions and edit_distance(word, suggestions[0]) <= max_dist:
+        return suggestions[0]
+    else:
+        return word
+
+def tokenize(text):
+    # remove non letters
+    text = re.sub("[^a-zA-Z]", " ", text)
+    # tokenize
+    tokens = nltk.word_tokenize(text)
+    # correct spell
+    words = sp_correct(tokens)
+    # stem
+    stems = stem_tokens(words, stemmer)
+    return stems
+
+stop_words = stopwords.words('english')
+# remove http,rt etc
+stop_words.append(u'http')
+stop_words.append(u'rt')
+stop_words.append(u'www')
+stop_words.append(u'com')
+stop_words.append(u'quot')
+######## 
+
+vectorizer = CountVectorizer(
+    analyzer = 'word',
+    tokenizer = tokenize,
+    lowercase = True,
+    stop_words = stop_words,
+    max_features = 100
+)
+ #fit the model and learns the vocabulary;
+ #transform our corpus data into feature vectors.
+corpus_data_features = vectorizer.fit_transform(
+    train_data_df.SentimentText.tolist())
+
+# Transfrom count to term frequency
+tfidf_transformer = TfidfTransformer()
+corpus_data_tfidf = tfidf_transformer.fit_transform(corpus_data_features)
+#corpus_data_tfidf.shape
+
+#Split arrays into random train and test subsets
+X_train, X_test, y_train, y_test  = train_test_split(
+        corpus_data_tfidf, 
+        train_data_df.Sentiment,
+        train_size=0.85, 
+        random_state=1234)
+
+#Train the classifier
+#NB_model= MultinomialNB().fit(corpus_data_tfidf,  train_data_df.Sentiment)
+NB_model = MultinomialNB().fit(X_train, y_train)
+
+#test and classfier and evaluate
+y_pred = NB_model.predict(X_test)
+scores = NB_model.score(X_test, y_test)
+c_scores = cross_val_score(NB_model, 
+                           corpus_data_tfidf, 
+                           train_data_df.Sentiment )
+print(classification_report(y_test, y_pred))
+print "scores:",scores
+print("Accuracy: %0.2f (+/- %0.2f)" % (c_scores.mean(), scores.std() * 2))
+
+# sample some of them
+spl = random.sample(xrange(len(y_pred)), 15)
+
+# print text and labels
+for text, sentiment in zip(train_data_df.SentimentText[spl], y_pred[spl]):
+    print sentiment, text
---
+++ Py_clf_evl/evaluation_SVC.py
@@ -0,0 +1,126 @@
+import json
+import pandas as pd
+import re, nltk
+import numpy as np
+import pickle
+import enchant
+import random
+from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.metrics import edit_distance
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer  
+from sklearn.cross_validation import train_test_split  
+from sklearn.svm import LinearSVC
+from sklearn.metrics import classification_report
+from sklearn.model_selection import cross_val_score    
+    
+
+
+tweets_data_path = '/home/candace/Documents/sentiment_analysis_dataset.csv'
+classifier_LinearSVC_path = '/home/candace/Documents/LinearSVC.pickle'
+vectorizer_CountVectorizer_path = '/home/candace/Documents/CountVectorizer.pickle'
+vectorizer_TfidfVectorizer_path = '/home/candace/Documents/TfidfVectorizer.pickle'
+
+whole_data_df = pd.read_csv(tweets_data_path, error_bad_lines=False)
+train_data_df = whole_data_df[['Sentiment','SentimentText']]
+
+# based on http://scikit-learn.org/stable/modules/generated/sklearn.svm.LinearSVC.html
+stemmer = PorterStemmer()
+def stem_tokens(tokens, stemmer):
+    stemmed = []
+    for item in tokens:
+        stemmed.append(stemmer.stem(item))
+    return stemmed
+    
+dt = enchant.Dict("en")
+max_dist = 2
+def sp_correct(word):
+    if dt.check(word):
+        return word
+    suggestions = dt.suggest(word)
+    if suggestions and edit_distance(word, suggestions[0]) <= max_dist:
+        return suggestions[0]
+    else:
+        return word
+
+def tokenize(text):
+    # remove non letters
+    text = re.sub("[^a-zA-Z]", " ", text)
+    # tokenize
+    tokens = nltk.word_tokenize(text)
+    # correct spell
+    words = sp_correct(tokens)
+    # stem
+    stems = stem_tokens(words, stemmer)
+    return stems
+
+stop_words = stopwords.words('english')
+# remove http,rt etc
+stop_words.append(u'http')
+stop_words.append(u'rt')
+stop_words.append(u'www')
+stop_words.append(u'com')
+stop_words.append(u'quot')
+######## 
+
+##Defining a vectorizer for feature extraction
+#vectorizer = CountVectorizer(
+#    analyzer = 'word',
+#    tokenizer = tokenize,
+#    lowercase = True,
+#    stop_words = stop_words,
+#    max_features = 100
+#)
+
+#equals to cv+transform
+vectorizer = TfidfVectorizer(
+    analyzer = 'word',
+    tokenizer = tokenize,
+    lowercase = True,
+    stop_words = stop_words,
+    max_features = 100
+)
+
+ #fit the model and learns the vocabulary;
+ #transform our corpus data into feature vectors.
+corpus_data_features = vectorizer.fit_transform(
+    train_data_df.SentimentText.tolist())
+    
+#convert the result to an array
+corpus_data_features_nd = corpus_data_features.toarray()
+#corpus_data_features_nd.shape
+
+#Words in the feature vocabulary
+vocab = vectorizer.get_feature_names()
+print vocab
+
+#Split arrays into random train and test subsets
+X_train, X_test, y_train, y_test  = train_test_split(
+        corpus_data_features_nd, 
+        train_data_df.Sentiment,
+        train_size=0.85, 
+        random_state=1234)
+
+#Train the classifier and evaluation
+#SVC_model= LinearSVC(C=0.5).fit(corpus_data_features_nd, train_data_df.Sentiment)
+SVC_model = LinearSVC(C=0.5).fit(X_train, y_train)
+
+#test and classfier and 
+y_pred = SVC_model.predict(X_test)
+scores = SVC_model.score(X_test, y_test)
+c_scores = cross_val_score(SVC_model, 
+                           corpus_data_features_nd, 
+                           train_data_df.Sentiment )
+print(classification_report(y_test, y_pred))
+print "scores:",scores
+print("Accuracy: %0.2f (+/- %0.2f)" % (c_scores.mean(), scores.std() * 2))
+
+# sample some of them
+spl = random.sample(xrange(len(y_pred)), 15)
+
+# print text and labels
+for text, sentiment in zip(train_data_df.SentimentText[spl], y_pred[spl]):
+    print sentiment, text
+
